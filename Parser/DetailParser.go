bdfa000674ec95cd91bf615c39d37a3ee728d7cc
Payne <1289690157@qq.com>
2021-03-10 21:50:04 +0800

    upload

--- Parser/DetailParser.go
+++ Parser/DetailParser.go
@@ -1,18 +1,38 @@
 package Parser
 
-import "fmt"
-
-// RegexParseDetail
-func RegexParseDetail(body []byte) {
-	fmt.Printf("RegexParseDetail:%s", body)
+type BookDetail struct {
+	Title            string // 标题
+	Author           string // 作者
+	PublicationHouse string // 出版社
+	Pages            string // 页数
+	Price            string // 价格
+	Binding          string // 装帧
+	Series           string // 系列
+	ISBN             string // 书号
+	Score            string // 评分
+	Translator       string // 译者
+	Introduction     string // 简介
+	Catalog          string // 目录
 }
 
 // QueryParseDetail
-func QueryParseDetail(body []byte) {
-	fmt.Printf("QueryParseDetail:%s", body)
-}
+//func QueryParseDetail(content []byte) BookDetail {
+//	bookdetail := BookDetail{}
+//	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(content))
+//	if err != nil {
+//		log.Printf("NewDocumentFromReader err: %s", err)
+//	}
+//	//doc.Find()
+//	return bookdetail
+//}
 
 // XpathParseDetail
-func XpathParseDetail(body []byte) {
-	fmt.Printf("XpathParseDetail:%s", body)
-}
+//func XpathParseDetail(content []byte) BookDetail {
+//	bookdetail := BookDetail{}
+//	doc, err := htmlquery.Parse(bytes.NewBuffer(content))
+//	if err != nil {
+//		fmt.Printf("htmlquery Parse Error: %s", err)
+//	}
+//	nodes := htmlquery.Find(doc, ``)
+//
+//}
--- main.go
+++ main.go
@@ -1,41 +1,23 @@
 package main
 
 import (
-	"C"
 	"fmt"
-	"github.com/payne/awesomeCrawler/Engine"
-	"github.com/payne/awesomeCrawler/Parser"
 )
 
-//export Example
-func Example() {
-	tagURL := `https://book.douban.com/tag/`
-	ListURL := `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`
-	fmt.Println(tagURL, ListURL)
-	Engine.Run(Engine.Request{
-		Method: "GET",
-		URL:    ListURL,
-		//URL:    `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`,
-		//ParseFunc: Parser.CssParseTag,
-		//ParseFunc: Parser.QueryParseList,
-		ParseFunc: Parser.XpathParseList,
-	})
-}
-
 func main() {
-	tagURL := `https://book.douban.com/tag/`
-	ListURL := `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`
-	fmt.Println(tagURL, ListURL)
-	Engine.Run(Engine.Request{
-		Method: "GET",
-		URL:    ListURL,
-		//URL:    `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`,
-		//ParseFunc: Parser.CssParseTag,
-		//ParseFunc: Parser.QueryParseList,
-		ParseFunc: Parser.XpathParseList,
-	})
-	for i := 0; i < count; i++ {
-		
+	// tagURL := `https://book.douban.com/tag/`
+	// ListURL := `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`
+	// fmt.Println(tagURL, ListURL)
+	// Engine.Run(Engine.Request{
+	// 	Method: "GET",
+	// 	URL:    ListURL,
+	// 	//URL:    `https://book.douban.com/tag/%E5%B0%8F%E8%AF%B4`,
+	// 	//ParseFunc: Parser.CssParseTag,
+	// 	//ParseFunc: Parser.QueryParseList,
+	// 	ParseFunc: Parser.XpathParseList,
+	// })
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 	}
 
 }
